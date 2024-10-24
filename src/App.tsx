85904107cfbc84688d9f452239517963f7453038
Sasha Karpovich <roobel2005@gmail.com>
2024-10-24 09:57:11 +0300

    wip: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div>
+      <div>1234</div>
+    </div>
+  );
+};
+
+App.displayName = "App";
---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@songbird": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      name: "big-screen-components",
+      entry: {
+        index: "./src/App.tsx",
+       },
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+  },
+});
